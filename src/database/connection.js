14c822208a50d98bca48bdbca4df4b53a4f89ee4
JullyanoBortolonSchuhmacher <jullyano_schuhmac@estudante.sesisenai.org.br>
2024-07-06 17:04:26 -0300

    coloque a senha 🤺

--- src/database/connection.js
+++ src/database/connection.js
@@ -6,7 +6,7 @@ class Database {
             user: 'postgres',     
             host: 'localhost',        
             database: 'carrinho_de_compras',  
-            password: '6535',     
+            password: 'sua_senha',     
             port: 5432,                
         });
     }
